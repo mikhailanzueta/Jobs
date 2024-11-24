9256946a47f91f737fe0f28c134e3f5736b61969
Mikhail Anzueta <mikianzueta@gmail.com>
2024-11-24 16:54:21 -0500

    Ticket 1: Created a function to read and parse yaml file data into a dictionary and return the data.

--- jobs-fetching-service/main.py
+++ jobs-fetching-service/main.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
-
+import yaml
+print('Hello world')
 class ParsedData:
     pass
 
 # Ticket #1 - return yaml configs from file as a python dictionary
+
 def read_yaml_from_file(file_name: str) -> dict:
-    pass
+    with open(file_name, 'r') as read_file:
+        configs = yaml.safe_load(read_file)
+    return configs
+file_name = 'Indeed.yaml'
+contents = read_yaml_from_file(file_name)
+print(contents)
 
 # Ticket #3 - return html from fetched site as a data structure
 def fetch_html_from_site(url: str, endpoint: str, query: str) -> BeautifulSoup:
